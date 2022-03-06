f338808bed0d3c87a21d91c67b84b03d06435d78
MartijndenHoed <82953946+MartijndenHoed@users.noreply.github.com>
2022-03-06 15:00:36 +0100

    Delete tekstScript1.js

--- paasdiner/tekstScript1.js
+++
@@ -1,9 +0,0 @@
-//alert("hi");
-function room = {
-	this.intro = "Je opent je ogen moeizaam, slaapophopingen van de afgelopen weken drukken op je oogleden en het felle licht brandt op je netvlies. "
-	
-	
-}
-
-
-var rooms[] = {};

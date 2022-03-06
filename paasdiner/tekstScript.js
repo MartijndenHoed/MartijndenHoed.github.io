48612823eee2638d4075725ec9c0f26f6a2e9852
MartijndenHoed <82953946+MartijndenHoed@users.noreply.github.com>
2022-03-06 14:57:44 +0100

    Update tekstScript.js

--- paasdiner/tekstScript.js
+++ paasdiner/tekstScript.js
@@ -1,6 +1,6 @@
 
 player.room = world.rooms[0];
-textParser.displayText("Welkom bij de interactieve paasdiner ervaring. Type commandos om acties uit te voeren, type help voor een lijst van alle commandos",0);
+textParser.displayText("Welkom bij de interactieve paasdiner text-adventure. Type commandos om acties uit te voeren, type help voor een lijst van alle commandos",0);
 textParser.displayText("Je wordt verdwaast wakker uit een diepe slaap. Aan de geur van kots merk je dat je waarschijnlijk iets te brokko bent gegaan en lichtelijk coma ging.",1)
 player.room.load();
 

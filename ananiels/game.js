8de39e2e561f6b24e67bc8bb2785244377a43aa2
MartijndenHoed <82953946+MartijndenHoed@users.noreply.github.com>
2024-03-08 11:12:29 +0100

    Add files via upload

---
+++ ananiels/game.js
@@ -0,0 +1,61 @@
+challenges =[
+{
+/*
+type:"normal",
+title: "",
+text: "",
+image: "",
+},
+*/
+type:"normal",
+title: "bak",
+text: "#PLAYER neemt een (wijn)bak",
+image: "",
+},
+{
+	type:"virus",
+title: "bak",
+endText: "#PLAYER virus2",
+startText: "#PLAYER virus1",
+rounds:2,
+image: "",
+},
+
+//slokken voor laatste quote
+//iedereen met een bak tegoed moet er een inlossen
+// 3 slokken voor iedereen die er vorige keer niet was
+// tussen speler en speler met slechtste haarlijn moet slokken
+// als speler geen pisshottje heeft gedaan, doe shotje
+// iedereen die vorige filmavond eten eheft besteld 5 slokken.
+// alle priesters nemen 3 slokken
+// virus: elk zelfstandig naamwoord dat speler zegt moet eindigen met -dam
+//virus: elke keer dat je naar bas refereerd moet je hem een groomer noemen
+// als nielsopdam in het afgelopen halfuur met murron heeft geappt, doet speler 3 slokken
+// voor elke week dat jeroen er al niet is doen een slokken
+// virus persoon moet klappen als koen tot het virus voorbij is
+// iederen neemt 1 slok van zelf meegenomen drank
+// persoon gaat op persoon zitten en roept 'mama ga van me af'
+// virus persoon heeft alcoholverbod
+// iedereen sluit een kotspact of trek een bak
+// virus achter elke zin moet speler huhu zeggen
+//als je ooit blijles les waar je blij van wordt hebt gegeven, doe 1 slok
+//spel noem omstebeurt iets kils aan koen's woonkamer, 8 slokken voor verliezer
+//spel noem alle nummers van niels tot herhaling
+// speler en speler, laagste in beklijst wint
+//stuur naar laatste persoon dat je poept onder de douche
+// eet als beschikbaar een stuk fruit met schil (3 happen) of 6 slokken
+// persoon  laat aan persoon bilspleet zien als opdam ander 6 slokken
+//virus ga als opdam plat liggen tot voorbij
+//virus elke keer als je niet max op een max zegt moet je een bak doen
+//virus iedereen elk woord dat eindigt op -ing wordt ings of 3 slokken
+//als je nekbaard hebt 5 slokken
+//als je deze avond kanker hebt geroepen doe je een bak
+//persoon maakt sapje (3 ingredienten) voor persoon niet slokt 8 slokken
+//hou een speech van 1 minuut over je werk
+//persoon wijst een milf aan, milf's zoon drinkt 3 slokken
+// iedereen die zijn verjaardag nog niet heeft gevierd 5 slokken
+// pak om half tien de bus voor minimaal 1 halte
+//zeg de abc formule uit je hoofd
+
+
+]

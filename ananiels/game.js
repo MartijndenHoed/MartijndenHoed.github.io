5cad30799cb20ffc18b2999a2f4127acbdc244bd
MartijndenHoed <82953946+MartijndenHoed@users.noreply.github.com>
2024-03-16 13:39:12 +0100

    Add files via upload

--- ananiels/game.js
+++ ananiels/game.js
@@ -1,61 +1,466 @@
 challenges =[
-{
-/*
-type:"normal",
-title: "",
-text: "",
-image: "",
-},
-*/
-type:"normal",
-title: "bak",
-text: "#PLAYER neemt een (wijn)bak",
-image: "",
-},
-{
-	type:"virus",
-title: "bak",
-endText: "#PLAYER virus2",
-startText: "#PLAYER virus1",
-rounds:2,
-image: "",
-},
-
-//slokken voor laatste quote
-//iedereen met een bak tegoed moet er een inlossen
-// 3 slokken voor iedereen die er vorige keer niet was
-// tussen speler en speler met slechtste haarlijn moet slokken
-// als speler geen pisshottje heeft gedaan, doe shotje
-// iedereen die vorige filmavond eten eheft besteld 5 slokken.
-// alle priesters nemen 3 slokken
-// virus: elk zelfstandig naamwoord dat speler zegt moet eindigen met -dam
-//virus: elke keer dat je naar bas refereerd moet je hem een groomer noemen
-// als nielsopdam in het afgelopen halfuur met murron heeft geappt, doet speler 3 slokken
-// voor elke week dat jeroen er al niet is doen een slokken
-// virus persoon moet klappen als koen tot het virus voorbij is
-// iederen neemt 1 slok van zelf meegenomen drank
-// persoon gaat op persoon zitten en roept 'mama ga van me af'
-// virus persoon heeft alcoholverbod
-// iedereen sluit een kotspact of trek een bak
-// virus achter elke zin moet speler huhu zeggen
-//als je ooit blijles les waar je blij van wordt hebt gegeven, doe 1 slok
-//spel noem omstebeurt iets kils aan koen's woonkamer, 8 slokken voor verliezer
-//spel noem alle nummers van niels tot herhaling
-// speler en speler, laagste in beklijst wint
-//stuur naar laatste persoon dat je poept onder de douche
-// eet als beschikbaar een stuk fruit met schil (3 happen) of 6 slokken
-// persoon  laat aan persoon bilspleet zien als opdam ander 6 slokken
-//virus ga als opdam plat liggen tot voorbij
-//virus elke keer als je niet max op een max zegt moet je een bak doen
-//virus iedereen elk woord dat eindigt op -ing wordt ings of 3 slokken
-//als je nekbaard hebt 5 slokken
-//als je deze avond kanker hebt geroepen doe je een bak
-//persoon maakt sapje (3 ingredienten) voor persoon niet slokt 8 slokken
-//hou een speech van 1 minuut over je werk
-//persoon wijst een milf aan, milf's zoon drinkt 3 slokken
-// iedereen die zijn verjaardag nog niet heeft gevierd 5 slokken
-// pak om half tien de bus voor minimaal 1 halte
-//zeg de abc formule uit je hoofd
-
-
+        {
+        "type": "game",
+        "title": "Ochtendgymnastiek",
+        "text": "#PLAYER verzint een balansoefening, iedereen die deze oefening minder lang volhoudt dan #PLAYER neemt 4 slokken",
+        "image": null
+    },
+    {
+        "type": "game",
+        "title": "de Nummers",
+        "text": "Iedereen noemt om de beurt een nummer van Opdam, bij niet meer weten of herhaling neemt de verliezer 6 slokken",
+        "image": null
+    },
+    {
+        "type": "game",
+        "title": "Kille woonkamer",
+        "text": "Iedereen noemt om de beurt iets kils aan Koens woonkamer, bij niet meer weten of herhaling neemt de verliezer 6 slokken",
+        "image": null
+    },
+    {
+        "type": "game",
+        "title": "Rotterdam Centraal",
+        "text": "Iedereen noemt om de beurt een station waar Marco nog moet stranden, bij niet meer weten of herhaling neemt de verliezer 6 slokken",
+        "image": null
+    },
+	{
+        "type": "normal",
+        "title": "Onder de douche",
+        "text": "#PLAYER, stuur naar je bovenste WhatsApp contact dat je onder de douche hebt gepoept of neem 5 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Quotes",
+        "text": "De persoon die het laatste is gequote neemt 3 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Openstaande bakken",
+        "text": "Iedereen die een openstaande bak heeft staan moet er nu 1 inlossen",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Bijgewoond door elk lid",
+        "text": "Iedereen die er vorige filmavond niet bij was neemt 3 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Haarlijn",
+        "text": "#PLAYER en #OTHERPLAYER, degene met de slechtste haarlijn neemt 3 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Pisshotje",
+        "text": "#PLAYER, als je nog nooit een pisshotje hebt gedronken, neem nu een shotje van min. 15%",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Thuisbezorgd",
+        "text": "Iedereen die vorige filmavond eten heeft besteld krijgt 5 slokken thuisbezorgd",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Priesteraat Natuurlijk Staat",
+        "text": "Iedere priester van de Natuurlijke Staat neemt 3 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Smaaktest",
+        "text": "Iedereen neemt een slok van elke door zichzelf meegenomen fles, als jouw fles al op is mag je per fles 2 slokken uitdelen",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Tiran",
+        "text": "#PLAYER, laat #OTHERPLAYER op je zitten en roep 'Mama ga van me af!' of neem 3 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Kotspact",
+        "text": "Iedereen, sluit een kotspact of neem 8 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "BLIJLES",
+        "text": "Iedereen die ooit 'BLIJLES: les waar je blij van wordt' heeft gegeven neemt 3 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Bekcompetitie",
+        "text": "#PLAYER en #OTHERPLAYER, degene die lager in de bekcompetitie staat van DIT JAAR neemt 5 slokken, bij een gelijkspel mogen beide spelers 5 slokken uitdelen",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Bekcompetitie",
+        "text": "#PLAYER en #OTHERPLAYER, degene die lager in de bekcompetitie staat IN TOTAAL neemt 5 slokken, bij een gelijkspel mogen beide spelers 5 slokken uitdelen",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Fruit met schil",
+        "text": "#PLAYER, eet als beschikbaar een stuk fruit waar je dat normaal niet zou doen met schil (min. drie happen) of neem 6 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Bilspleet",
+        "text": "#PLAYER, laat zoals Opdam zou doen aan #OTHERPLAYER je bilspleet zien of neem 7 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Nekbaard",
+        "text": "Iedereen met een nekbaard neemt 4 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Kaneel",
+        "text": "Iedereen die deze avond met kanker heeft geroepen trekt een bak",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Sapje",
+        "text": "#PLAYER, neem een slok van een sapje gemaakt door #OTHERPLAYER (max. drie nonalcoholische ingrediënten) of neem 7 slokken, als het je lukt neemt #OTHERPLAYER 7 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Boomer",
+        "text": "#PLAYER, houdt een speech van 2 minuten over je werk, als je stilvalt neem je 5 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "MILFs",
+        "text": "#PLAYER, wijs de persoon met de beste MILF aan, die persoon neemt 4 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Verjaardag",
+        "text": "Iedereen die (nog) niet zijn vorige verjaardag heeft gevierd neemt 5 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Half tien laatste bus",
+        "text": "Pak om half tien of zsm. daarna de laatste bus voor minimaal 1 halte of neem 8 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "abc-formule",
+        "text": "#PLAYER, zeg de abc-formule uit je hoofd of neem 4 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Goblin mode",
+        "text": "#PLAYER, ga goblin mode of neem 3 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Larsgrap",
+        "text": "#PLAYER en #OTHERPLAYER, vertel beide een larsgrap aan de groep, degene met de grappigste grap neemt 5 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Stoomkosten",
+        "text": "#PLAYER, betaal de host tikkie voor stroomkosten of neem drie slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Oorlikken",
+        "text": "#PLAYER, ga #OTHERPLAYER oorlikken, degene die weigert neemt 6 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Alternatieve rizz",
+        "text": "#PLAYER en #OTHERPLAYER, vertel beide een alternatieve rizz pickup line, degene met de slechte pickup line neemt 6 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Neus",
+        "text": "Iedereen die een grotere neus heeft van #PLAYER neemt 2 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Kleine speler huis",
+        "text": "Iedereen die in het kleine speler huis sliep 3 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Grote speler huis",
+        "text": "Iedereen die in het grote speler huis sliep 3 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Overhemd",
+        "text": "Iedereen in overhemd neemt 5 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Spaghettiplantsoen",
+        "text": "Iedereen wiens partner deze avond heeft gebarft neemt 5 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "DINER BESTAAT",
+        "text": "#PLAYER en #OTHERPLAYER, verzin een compleet orgineel datediner theme, degene met het slechte thema neemt 6 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "How to Kiss",
+        "text": "#PLAYER, leg de rest uit hoe je moet zoenen of neem 3 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Hele jaar bruin",
+        "text": "#PLAYER en #ORHERPLAYER, degene die minder hele jaar bruin is neemt 4 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Banaan",
+        "text": "#PLAYER, imiteer Lars de orang-oetan bij de banaan, #OTHERPLAYER",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Just Dance",
+        "text": "#PLAYER, dans zoals Koen bij Just Dance of neem 3 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Optillen",
+        "text": "#PLAYER, til een slapende #OTHERPLAYER op of neem 6 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Lindes kaak",
+        "text": "#PLAYER, laat je kaak breken door #OTHERPLAYER of neem 7 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Maraghosten",
+        "text": "#PLAYER, ghost elk WhatsApp gesprek voor de rest van de avond of neem 8 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Hangjongeren",
+        "text": "#PLAYER, blijf 20 seconden met je volle lichaamsgewicht hangen aan een object of neem 6 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Kwetsbaar",
+        "text": "Iedereen die kwetsbaar (kleiner) is voor #PLAYER neemt drie slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Adtje voor de sfeer",
+        "text": "#PLAYER doet een adtje voor de sfeer",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Martijns jukebox",
+        "text": "#PLAYER zingt een nummer gekozen door #OTHERPLAYER mee zonder de tekst te zien, omdat Martijns jukebox nog steeds niet gefixt is, bij elke fout 1 slok",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Incel",
+        "text": "#PLAYER en #OTHERPLAYER, verzin een incelopmerking, degene met de minst incel opmerking neemt 6 slokken",
+        "image": null
+    },
+    {
+        "type": "normal",
+        "title": "Hoekbank",
+        "text": "Iedereen die niet in de hoek van een bank zit neem 2 slokken",
+        "image": null
+    },
+	{
+        "type": "virus",
+        "title": "Dammificatie",
+        "startText": "Iedereen moet elk zelfstandig naamwoord eindigen met -dam, bij falen 1 slok",
+        "endText": "Iedereendam mag stoppen met overal -dam achter zetten",
+        "rounds": "3",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Groomer",
+        "startText": "Iedereen mag alleen naar Bas refereren met het woord 'groomer', bij falen 3 slokken",
+        "endText": "Groomers prooi is 18 geworden, iedereen mag stoppen met Bas 'groomer' noemen",
+        "rounds": "5",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Klappen",
+        "startText": "#PLAYER moet klappen en de naam van #OTHERPLAYER blijven herhalen, bij falen 1 slok",
+        "endText": "#OTHERPLAYER is genoeg aangemoedigd en moet 5 slokken nemen, #PLAYER mag stoppen met klappen",
+        "rounds": "4",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Alcoholverbod",
+        "startText": "#PLAYER krijgt van moeder een alcoholverbod, dus slokken mogen niet genomen worden",
+        "endText": "Het alcoholverbod van #PLAYER is voorbij",
+        "rounds": "4",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Hue hue",
+        "startText": "#PLAYER moet elke zin eindigen met 'hue hue', bij falen 1 slok",
+        "endText": "#PLAYER is uitgelachen en mag stoppen met 'hue hue' zeggen",
+        "rounds": "4",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Ings",
+        "startText": "Iedereen moet elk woord dat eindigt op -ing eindigen met -ings, bij falen 3 slokken",
+        "endText": "Iedereen mag stoppen met overal -ings van maken",
+        "rounds": "5",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Platliggen",
+        "startText": "#PLAYER heeft rugpijn en moet platliggen",
+        "endText": "#PLAYER heeft een Red-Bull genomen en hoeft niet meer te plattliggen",
+        "rounds": "4",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Max...?",
+        "startText": "Iedereen moet bij elke vermelding van Max 'Max...?' roepen, bij falen 3 slokken",
+        "endText": "Max is van een flat gesprongen (iedereen mag stoppen met 'Max...?' roepen)",
+        "rounds": "5",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Me-time",
+        "startText": "#PLAYER heeft me-time nodig en zondert zichzelf af van de groep, bij vroegtijdig terugkomen 8 slokken",
+        "endText": "#PLAYER heeft genoeg me-time gehad en mag terugkomen",
+        "rounds": "3",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Hoi, ik ben Martijn",
+        "startText": "#PLAYER, stel jezelf voor als Martijn aan iedereen wie je aanspreekt, bij falen 2 slokken",
+        "endText": "Iedereen weet eindelijk wie je bent Martijn, je mag stoppen met jezelf voorstellen",
+        "rounds": "4",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Flirten",
+        "startText": "Iedereen zit met de armen naar achteren om te flirten met Dena, bij falen 3 slokken, behalve om te drinken",
+        "endText": "Dena is te volwassen voor jou, je mag stoppenet flirten",
+        "rounds": "5",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Zet op pauze!",
+        "startText": "Iedereen moet voor elke vrouw die die ziet 'zet op pauze roepen!', bij falen 3 slokken",
+        "endText": "Alle vrouwen zijn op pauze gezet, iedereen mag stoppen met 'zet op pauze!' roepen",
+        "rounds": "6",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Kiki bestaat niet",
+        "startText": "#PLAYER bestaat niet en wordt dus door iedereen genegeerd, bij falen 3 slokken",
+        "endText": "Iemand heeft een foto laten zien van #PLAYER dus iedereen mag stoppen met negeren",
+        "rounds": "4",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Naaktfoto",
+        "startText": "#PLAYER, trek je shirt uit of neem 5 slokken",
+        "endText": "Er zijn genoeg foto's gemaakt, #PLAYER mag weer het shirt aan",
+        "rounds": "3",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Jeroens lach",
+        "startText": "#PLAYER, laat Jeroen niet al het werk alleen doen en trek een brede glimlach, bij falen 2 slokken",
+        "endText": "Je kaak is genoeg verkrampt #PLAYER, je mag stoppen met glimlachen",
+        "rounds": "6",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Doorpakken is doorpakken",
+        "startText": "#PLAYER, doorpakken is doorpakken, dus neem elke slok dubbel",
+        "endText": null,
+        "rounds": "4",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Annemijn",
+        "startText": "#PLAYER, blijf #OTHERPLAYER aanstaren alsof je net voor een datediner bent gevraagd, bij falen 3 slokken",
+        "endText": "#PLAYER, je schuift langzaam achter je bureau en hoeft #OTHERPLAYER dus niet meer aan te staren",
+        "rounds": "4",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Christel en Inez",
+        "startText": "#PLAYER en #OTHERPLAYER, jullie zijn vanaf nu dezelfde persoon en mogen elkaars zijde niet meer verlaten en drinken samen slokken, bij falen 5 slokken",
+        "endText": "#PLAYER en #OTHERPLAYER hebben hun eigen persoonlijkheid opgebouwd en mogen weer elkaars zijde verlaten",
+        "rounds": "5",
+        "image": null
+    },
+    {
+        "type": "virus",
+        "title": "Jeroen is uit Ananiels",
+        "startText": "#PLAYER, sta op een willekeurig moment in het spel op en roep 'Jeroen is uit Ananiels!', de speler die het laatste opstaat en juicht moet 5 slokken nemen, dit mag je 1 keer doen",
+        "endText": "geen einde?",
+        "rounds": "1000",
+        "image": null
+    }
 ]
